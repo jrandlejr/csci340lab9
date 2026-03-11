c271f626b2d6e8b64d86b4d3a581c93ee6f09656
jrandlejr <randleja@hendrix.edu>
2026-03-11 12:42:34 -0500

    Ran the following .NET CLI commands to install required NuGet packages

--- ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ffdfec7e0252dad7fbc111c574ef9f3be930e61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2391557dec81b1c35301ab0eaba5e54e5cfd058d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
