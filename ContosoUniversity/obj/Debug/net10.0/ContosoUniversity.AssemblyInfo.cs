2b14f027fa6938d246fb22b6a70e256f2988a0ca
jrandlejr <randleja@hendrix.edu>
2026-03-11 12:38:01 -0500

    Removed <Nullable>enable</Nullable> o eliminate the warnings

--- ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c32995fe58f07c12ad9c4eda5bef009d6e8a4ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ffdfec7e0252dad7fbc111c574ef9f3be930e61")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
