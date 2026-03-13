a0cb049a69c01cc42eb41e5ef34501bf1930e498
jrandlejr <randleja@hendrix.edu>
2026-03-13 12:30:46 -0500

    Updating the details page

--- ContosoUniversity/Pages/Students/Details.cshtml.cs
+++ ContosoUniversity/Pages/Students/Details.cshtml.cs
@@ -21,23 +21,24 @@ namespace ContosoUniversity.Pages.Students
 
         public Student Student { get; set; } = default!;
 
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+       public async Task<IActionResult> OnGetAsync(int? id)
+{
+    if (id == null)
+    {
+        return NotFound();
+    }
 
-            var student = await _context.Student.FirstOrDefaultAsync(m => m.ID == id);
+    Student = await _context.Students
+        .Include(s => s.Enrollments)
+        .ThenInclude(e => e.Course)
+        .AsNoTracking()
+        .FirstOrDefaultAsync(m => m.ID == id);
 
-            if (student is not null)
-            {
-                Student = student;
-
-                return Page();
-            }
-
-            return NotFound();
-        }
+    if (Student == null)
+    {
+        return NotFound();
+    }
+    return Page();
+}
     }
 }
