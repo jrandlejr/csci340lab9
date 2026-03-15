6a98392aa6271fa116567dc4046cd89755559e3c
jrandlejr <randleja@hendrix.edu>
2026-03-15 13:51:40 -0500

    Adding searching

--- ContosoUniversity/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -1,45 +1,58 @@
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
-
-public class IndexModel : PageModel
+namespace ContosoUniversity.Pages.Students
 {
-    private readonly SchoolContext _context;
-    public IndexModel(SchoolContext context)
+    public class IndexModel : PageModel
     {
-        _context = context;
-    }
-
-    public string NameSort { get; set; }
-    public string DateSort { get; set; }
-    public string CurrentFilter { get; set; }
-    public string CurrentSort { get; set; }
-
-    public IList<Student> Students { get; set; }
-
-    public async Task OnGetAsync(string sortOrder)
-    {
-        // using System;
-        NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
-        DateSort = sortOrder == "Date" ? "date_desc" : "Date";
-
-        IQueryable<Student> studentsIQ = from s in _context.Students
-                                        select s;
-
-        switch (sortOrder)
+        private readonly SchoolContext _context;
+        public IndexModel(SchoolContext context)
         {
-            case "name_desc":
-                studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
-                break;
-            case "Date":
-                studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
-                break;
-            case "date_desc":
-                studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
-                break;
-            default:
-                studentsIQ = studentsIQ.OrderBy(s => s.LastName);
-                break;
+            _context = context;
         }
 
-        Students = await studentsIQ.AsNoTracking().ToListAsync();
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+
+        public IList<Student> Students { get; set; }
+
+        public async Task OnGetAsync(string sortOrder, string searchString)
+{
+    NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+    DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+    CurrentFilter = searchString;
+
+    IQueryable<Student> studentsIQ = from s in _context.Students
+                                     select s;
+
+    if (!String.IsNullOrEmpty(searchString))
+    {
+        studentsIQ = studentsIQ.Where(s => s.LastName.Contains(searchString)
+                               || s.FirstMidName.Contains(searchString));
+    }
+
+    switch (sortOrder)
+    {
+        case "name_desc":
+            studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+            break;
+        case "Date":
+            studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
+            break;
+        case "date_desc":
+            studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
+            break;
+        default:
+            studentsIQ = studentsIQ.OrderBy(s => s.LastName);
+            break;
+    }
+
+    Students = await studentsIQ.AsNoTracking().ToListAsync();
+}
     }
 }
