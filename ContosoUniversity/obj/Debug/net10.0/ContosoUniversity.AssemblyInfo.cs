0ca5d1a0a61f2aaea185aded1dbae354fceebac5
jrandlejr <randleja@hendrix.edu>
2026-03-15 13:55:08 -0500

    Created PaginatedList.cs

--- ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97f01fa81c1d1c9a3d34f64ef90afe0420fa8383")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a98392aa6271fa116567dc4046cd89755559e3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
