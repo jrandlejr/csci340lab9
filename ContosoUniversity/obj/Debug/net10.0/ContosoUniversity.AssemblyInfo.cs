80433478a6ee3aa87640f1d77fe743e3451bc98b
jrandlejr <randleja@hendrix.edu>
2026-03-16 12:15:57 -0500

    Creating an Initial Migration

--- ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+855c5b6e4c241f715181c276cd83ef8ab9a800ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6fabe196308df0cf09fb264ad739f80acf6c5f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
