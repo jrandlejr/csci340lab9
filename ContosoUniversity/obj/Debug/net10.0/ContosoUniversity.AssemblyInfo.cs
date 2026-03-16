8ddefeaa1a9f3a824d809ea6b435a5ab77094e23
jrandlejr <randleja@hendrix.edu>
2026-03-16 15:34:52 -0500

    Added Age sorting to Student index page. Tested as well

--- ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+592357b21e7d4316f8aea3510ce6228712cfdccf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf081d364b4690362aa5772a048fb7d65d1152bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
