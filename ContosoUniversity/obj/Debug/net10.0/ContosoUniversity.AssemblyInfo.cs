cc8595c729872a5e569a62f20f276ee1b1cd7f78
jrandlejr <randleja@hendrix.edu>
2026-03-16 12:47:06 -0500

    Added Instructor and OfficeAssignment entities

--- ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05d107d04cfbb034004bd240c65bff7aa6574936")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70b957054c0bef7904cc5b12c59278ab976e7273")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
